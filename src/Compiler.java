86e4fb5030e86cc7ad480c29ad2a6e1edcff6468
SeanKohler <57458971+SeanKohler@users.noreply.github.com>
2022-01-28 14:42:31 -0500

    Now breaks program down into variables and some keywords

--- src/Compiler.java
+++ src/Compiler.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Compiler{
     public static void main(String[] args){
@@ -7,34 +8,44 @@ public class Compiler{
         }
         String filename = args[0];
         File file = new File(filename);
-
+        ArrayList<String>words = new ArrayList<String>();
+        
           try {
             Scanner input = new Scanner(file);
             int linenumber =0;
+            int lineposition=0;
             while (input.hasNextLine()){
                 String line = input.nextLine();
                 //System.out.println(line);
                 linenumber = linenumber+1;
-
+                String word="";
                 for(int i=0; i<line.length(); i++){
                     char character = line.charAt(i);
-                    if(Character.isWhitespace(character)){
-                       //Do nothing if whitespace is found 
+                    String charToString = String.valueOf(character);
+                    word = word + charToString.trim();
+                    //Test to see if character is a key char ex. (, ), {, }, =, +, !, $
+                    /*
+                    Code goes here
+                    */
+
+                    //Once we remove special characters out we should break into words based on spaces
+                    if(Character.isWhitespace(character)||charToString.equals("$")){
+                       if(!word.equals("")){
+                        words.add(word);
+                        System.out.println("DEBUG Lexer -["+word+"] Found At ("+linenumber+":"+i+")");
+                        word="";
+                       }
                     }else{
-                        System.out.println("DEBUG Lexer -["+character+"] Found At ("+linenumber+":"+i+")");
+                        //System.out.println("DEBUG Lexer -["+character+"] Found At ("+linenumber+":"+i+")");
                     }
                     
                 }
-
             }
             input.close();//Closes File after we read all contents
           }
           catch(Exception e) {
             System.out.println(e);
           }
-
-
-
-
     }
+
 }
