f635aa9131bb55b2a1ce40ee2d66471ae1039c13
SeanKohler <57458971+SeanKohler@users.noreply.github.com>
2022-01-28 23:36:29 -0500

    More correctly breaks down code. Also fixed formatting slightly

--- src/Compiler.java
+++ src/Compiler.java
@@ -9,36 +9,44 @@ public class Compiler{
         String filename = args[0];
         File file = new File(filename);
         ArrayList<String>words = new ArrayList<String>();
-        
+        int prognum=1;
           try {
             Scanner input = new Scanner(file);
             int linenumber =0;
             int lineposition=0;
+            //System.out.println("Starting Lex 1");
             while (input.hasNextLine()){
                 String line = input.nextLine();
-                //System.out.println(line);
                 linenumber = linenumber+1;
                 String word="";
                 for(int i=0; i<line.length(); i++){
                     char character = line.charAt(i);
                     String charToString = String.valueOf(character);
                     word = word + charToString.trim();
-                    //Test to see if character is a key char ex. (, ), {, }, =, +, !, $
-                    /*
-                    Code goes here
-                    */
-
-                    //Once we remove special characters out we should break into words based on spaces
-                    if(Character.isWhitespace(character)||charToString.equals("$")){
+                    //Test to see if character is a key char ex. (, ), {, }, =, +, !, <, >, $
+                    charToString = charToString.trim();
+                    if(specialChar(charToString, linenumber, i+1, true)==true){//This handles printing for all special characters
+                        word="";
+                    }else if(Character.isWhitespace(character)){//Once we remove special characters out we should break into words based on spaces
                        if(!word.equals("")){
                         words.add(word);
-                        System.out.println("DEBUG Lexer -["+word+"] Found At ("+linenumber+":"+i+")");
+                        System.out.println("DEBUG Lexer - ID [ "+word+" ] Found At ("+linenumber+":"+(i+1)+")");
                         word="";
                        }
                     }else{
-                        //System.out.println("DEBUG Lexer -["+character+"] Found At ("+linenumber+":"+i+")");
+                        char next = line.charAt(i+1);
+                        String convert = String.valueOf(next);
+                        if(specialChar(convert, linenumber, i, false)){//This checks if the next character is a special character
+                            if(!word.equals("")){
+                                words.add(word);
+                                System.out.println("DEBUG Lexer - ID [ "+word+" ] Found At ("+linenumber+":"+(i+1)+")");
+                                word="";
+                               }
+                        } 
                     }
-                    
+                    if(charToString.equals("$")){
+                        System.out.println();
+                    }
                 }
             }
             input.close();//Closes File after we read all contents
@@ -46,6 +54,36 @@ public class Compiler{
           catch(Exception e) {
             System.out.println(e);
           }
+    }
+    public static boolean specialChar(String character, int linenumber, int i, boolean print){
+        String text="";
+        if(character.equals("(")){
+            text="LeftParen";
+        }else if(character.equals(")")){
+            text="RightParen";
+        }else if(character.equals("{")){
+            text="LeftCurlBrace";
+        }else if(character.equals("}")){
+            text="RightCurlBrace";
+        }else if(character.equals("=")){
+            text="Assignment";
+        }else if(character.equals("+")){
+            text="Addition";
+        }else if(character.equals("!")){
+            text="Negation";
+        }else if(character.equals("<")){
+            text="LessThan";
+        }else if(character.equals(">")){
+            text="GreaterThan";
+        }else if(character.equals("$")){
+            text="EOP";
+        }else{
+            return false;
+        }
+        if(print ==true){//Sometimes we may want to check if a special character is next and not print it as the current character
+            System.out.println("DEBUG Lexer - "+text +" [ "+character+" ] Found At ("+linenumber+":"+i+")");
+        }
+        return true; 
     }
 
 }
